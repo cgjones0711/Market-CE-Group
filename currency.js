42a56f83948f3d8f9bbc04f6162955935639cd3f
Keona Kirby <keonakirby@gmail.com>
2021-03-29 11:24:40 -0500

    added currency converter api end point

---
+++ currency.js
@@ -0,0 +1,7 @@
+const URL = " https://v6.exchangerate-api.com/v6/c880451324935553df25617a/latest/EUR";
+
+fetch(URL)
+.then(response => response.json())
+.then(function(data){
+console.log(data);
+})
