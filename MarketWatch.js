60b3f07442ab7e16d78b2fcedef21f8928cfdf3d
Clay Jones <cgjones0711@gmail.com>
2021-03-31 09:33:03 -0500

    switched info to marketWatch

---
+++ MarketWatch.js
@@ -0,0 +1,197 @@
+let marketData = 
+
+
+{
+    "explains": [],
+    "count": 19,
+    "quotes": [
+        {
+            "exchange": "SNP",
+            "shortname": "S&P 500",
+            "quoteType": "INDEX",
+            "symbol": "^GSPC",
+            "index": "quotes",
+            "score": 436022,
+            "typeDisp": "Index",
+            "isYahooFinance": true
+        },
+        {
+            "exchange": "PCX",
+            "shortname": "SPDR S&P 500",
+            "quoteType": "ETF",
+            "symbol": "SPY",
+            "index": "quotes",
+            "score": 340145,
+            "typeDisp": "ETF",
+            "longname": "SPDR S&P 500 ETF Trust",
+            "isYahooFinance": true
+        },
+        {
+            "exchange": "CME",
+            "shortname": "E-Mini S&P 500 Jun 21",
+            "quoteType": "FUTURE",
+            "symbol": "ES=F",
+            "index": "quotes",
+            "score": 159695,
+            "typeDisp": "Future",
+            "isYahooFinance": true
+        },
+        {
+            "exchange": "PCX",
+            "shortname": "Vanguard S&P 500 ETF",
+            "quoteType": "ETF",
+            "symbol": "VOO",
+            "index": "quotes",
+            "score": 85760,
+            "typeDisp": "ETF",
+            "longname": "Vanguard S&P 500 ETF",
+            "isYahooFinance": true
+        },
+        {
+            "exchange": "NYQ",
+            "shortname": "Star Peak Energy Transition Cor",
+            "quoteType": "EQUITY",
+            "symbol": "STPK",
+            "index": "quotes",
+            "score": 75041,
+            "typeDisp": "Equity",
+            "longname": "Star Peak Energy Transition Corp.",
+            "isYahooFinance": true
+        },
+        {
+            "exchange": "PCX",
+            "shortname": "Direxion Daily S&P Biotech Bull",
+            "quoteType": "ETF",
+            "symbol": "LABU",
+            "index": "quotes",
+            "score": 64765,
+            "typeDisp": "ETF",
+            "longname": "Direxion Daily S&P Biotech Bull 3X Shares",
+            "isYahooFinance": true
+        },
+        {
+            "exchange": "NYQ",
+            "shortname": "Simon Property Group, Inc.",
+            "quoteType": "EQUITY",
+            "symbol": "SPG",
+            "index": "quotes",
+            "score": 62103,
+            "typeDisp": "Equity",
+            "longname": "Simon Property Group, Inc.",
+            "isYahooFinance": true
+        }
+    ],
+    "news": [
+        {
+            "uuid": "72051e7c-80f2-352b-8875-3f6ef1e3d17b",
+            "title": "We think the trend toward value stocks ‘will continue into 2021 and beyond’: Aspiriant Managing Director",
+            "publisher": "Yahoo Finance Video",
+            "link": "https://finance.yahoo.com/video/think-trend-toward-value-stocks-143618113.html",
+            "providerPublishTime": 1609425378,
+            "type": "VIDEO"
+        },
+        {
+            "uuid": "cd7baf4a-e37f-311c-bd37-edfb6b0363e1",
+            "title": "‘We are in a tale of two cities this year’: Ric Edelman on the state of businesses",
+            "publisher": "Yahoo Finance Video",
+            "link": "https://finance.yahoo.com/video/tale-two-cities-ric-edelman-163040871.html",
+            "providerPublishTime": 1609432240,
+            "type": "VIDEO"
+        },
+        {
+            "uuid": "0316f406-30e4-3f83-85e1-e18962807760",
+            "title": "What to expect for business in 2021",
+            "publisher": "Yahoo Finance Video",
+            "link": "https://finance.yahoo.com/video/expect-business-2021-180657697.html",
+            "providerPublishTime": 1609438017,
+            "type": "VIDEO"
+        },
+        {
+            "uuid": "5f4f7c5b-0b76-326f-9080-32e80893ac36",
+            "title": "2020 really ‘reinforced’ long-term investment strategies: president of SS&C ALPS Advisors",
+            "publisher": "Yahoo Finance Video",
+            "link": "https://finance.yahoo.com/video/2020-really-reinforced-long-term-183956820.html",
+            "providerPublishTime": 1609439996,
+            "type": "VIDEO"
+        },
+        {
+            "uuid": "8e3f39e3-7398-3a97-ad71-a7d27f2dc319",
+            "title": "Tech is ‘becoming a new wave for a lot of companies’ in 2021: chief market strategist",
+            "publisher": "Yahoo Finance Video",
+            "link": "https://finance.yahoo.com/video/tech-becoming-wave-lot-companies-192253634.html",
+            "providerPublishTime": 1609442573,
+            "type": "VIDEO"
+        },
+        {
+            "uuid": "0b0d1be0-2356-3dbc-9333-fd9ea238a25b",
+            "title": "We see value in the ‘dogs of the DOW’: Kendall Capital CEO",
+            "publisher": "Yahoo Finance Video",
+            "link": "https://finance.yahoo.com/video/see-value-dogs-dow-kendall-201405048.html",
+            "providerPublishTime": 1609445645,
+            "type": "VIDEO"
+        },
+        {
+            "uuid": "0332584e-4470-3e45-8dbd-9fb1d8e44856",
+            "title": "Georgia Senate runoffs, December jobs report: What to know in the week ahead",
+            "publisher": "Yahoo Finance",
+            "link": "https://finance.yahoo.com/news/georgia-senate-runoff-elections-december-jobs-report-what-to-know-in-the-week-ahead-161734507.html",
+            "providerPublishTime": 1609690654,
+            "type": "STORY"
+        },
+        {
+            "uuid": "dbab5508-9931-3ad2-b10c-eac9274602a7",
+            "title": "Centene to acquire Magellan Health in deal with enterprise value of $2.2 billion",
+            "publisher": "MarketWatch",
+            "link": "https://finance.yahoo.com/m/dbab5508-9931-3ad2-b10c-eac9274602a7/centene-to-acquire-magellan.html",
+            "providerPublishTime": 1609759140,
+            "type": "STORY"
+        }
+    ],
+    "nav": [],
+    "lists": [
+        {
+            "slug": "the-berkshire-hathaway-portfolio",
+            "name": "The Berkshire Hathaway Portfolio",
+            "index": "the-berkshire-hathaway-portfolio",
+            "score": 51.544605,
+            "type": "ALGO_WATCHLIST",
+            "brandSlug": "yahoo-finance",
+            "pfId": "the_berkshire_hathaway_portfolio"
+        },
+        {
+            "slug": "420_stocks",
+            "name": "Cannabis Stocks",
+            "index": "420_stocks",
+            "score": 13.607001,
+            "type": "ALGO_WATCHLIST",
+            "brandSlug": "yahoo-finance",
+            "pfId": "420_stocks"
+        },
+        {
+            "slug": "tech-stocks-that-move-the-market",
+            "name": "Tech Stocks That Move the Market",
+            "index": "tech-stocks-that-move-the-market",
+            "score": 11.284714,
+            "type": "ALGO_WATCHLIST",
+            "brandSlug": "yahoo-finance",
+            "pfId": "the_only_tech_stocks_that_matter"
+        },
+        {
+            "index": "7b9d1a1c-1fc6-4883-8844-bd39034da6a4",
+            "id": "7b9d1a1c-1fc6-4883-8844-bd39034da6a4",
+            "title": "Security & Protection Services",
+            "canonicalName": "SECURITY_PROTECTION_SERVICES",
+            "score": 1,
+            "type": "PREDEFINED_SCREENER"
+        }
+    ],
+    "researchReports": [],
+    "totalTime": 220,
+    "timeTakenForQuotes": 421,
+    "timeTakenForNews": 600,
+    "timeTakenForAlgowatchlist": 400,
+    "timeTakenForPredefinedScreener": 400,
+    "timeTakenForCrunchbase": 0,
+    "timeTakenForNav": 400,
+    "timeTakenForResearchReports": 0
+}
--- market.js
+++ market.js
@@ -2,31 +2,35 @@ let marqueeSelector = document.getElementById("marqueeSelector");
 
 
 
-fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=S%26P&region=US", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-key": "b7dd329672msh89d3093c2444b8ep1fce20jsn5949b7fbcaf1",
-		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
-	}
-})
-.then(response => response.json())
-.then(function(data){
-	console.log(data);
+// fetch("https://apidojo-yahoo-finance-v1.p.rapidapi.com/auto-complete?q=S%26P&region=US", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-key": "b7dd329672msh89d3093c2444b8ep1fce20jsn5949b7fbcaf1",
+// 		"x-rapidapi-host": "apidojo-yahoo-finance-v1.p.rapidapi.com"
+// 	}
+// })
+// .then(response => response.json())
+// .then(function(data){
+// 	manipulateData(data);
+
 	
-	for (let i = 0; i < data.news.length; i++) {
-		// console.log(data.news[i].title);
-		// console.log(data.news[i].link);
-		let newsEl = document.createElement("a");
-		let dotEl = document.createElement("span");
-		dotEl.classList.add("dot");
-		newsEl.textContent = data.news[i].title;
-		marqueeSelector.appendChild(dotEl);
-		marqueeSelector.appendChild(newsEl);
-	}
+// 	for (let i = 0; i < data.news.length; i++) {
+// 		// console.log(data.news[i].title);
+// 		// console.log(data.news[i].link);
+// 		let newsEl = document.createElement("a");
+// 		let dotEl = document.createElement("span");
+// 		dotEl.classList.add("dot");
+// 		newsEl.textContent = data.news[i].title;
+// 		marqueeSelector.appendChild(dotEl);
+// 		marqueeSelector.appendChild(newsEl);
+// 	}
 	
 	
 	
 
 
 
-})
+// })
+function manipulateData (marketData) {
+
+};
