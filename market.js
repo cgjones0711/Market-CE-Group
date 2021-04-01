fa865bcebe3e31af02a5e0942e0033f2b33e5d2c
Andrew Medina <andre.30001197@gmail.com>
2021-04-01 11:36:57 -0500

    style changes added charts

--- market.js
+++ market.js
@@ -39,77 +39,113 @@ let STPK=document.getElementById("STPK")
 
 
 // })
-function manipulateData (marketData) {
+// function manipulateData (marketData) {
 
-};
+// };
 
-SP500.addEventListener("click", function(event){
-    event.preventDefault();
+// SP500.addEventListener("click", function(event){
+//     event.preventDefault();
     
 
 
     
-container.innerHTML = `<h4>S&P 500</h4> 
-                        <p>    Quote Type: ${marketData.quotes[0].quoteType}</p>
-                        <p> Symbol:  ${marketData.quotes[0].symbol}</p>
-                        <p> Score: ${marketData.quotes[0].score} </p>`
+// container.innerHTML = `<h4>S&P 500</h4> 
+//                         <p>    Quote Type: ${marketData.quotes[0].quoteType}</p>
+//                         <p> Symbol:  ${marketData.quotes[0].symbol}</p>
+//                         <p> Score: ${marketData.quotes[0].score} </p>`
                     
 
 
-})
-SPDR.addEventListener("click", function(event){
-    event.preventDefault();
+// })
+// SPDR.addEventListener("click", function(event){
+//     event.preventDefault();
     
 
 
     
-container.innerHTML = `<h4>${marketData.quotes[1].longname}</h4> 
-                        <p> Quote Type: ${marketData.quotes[1].quoteType}</p>
-                        <p> Symbol:  ${marketData.quotes[1].symbol}</p>
-                        <p> Score: ${marketData.quotes[1].score} </p>`
+// container.innerHTML = `<h4>${marketData.quotes[1].longname}</h4> 
+//                         <p> Quote Type: ${marketData.quotes[1].quoteType}</p>
+//                         <p> Symbol:  ${marketData.quotes[1].symbol}</p>
+//                         <p> Score: ${marketData.quotes[1].score} </p>`
                     
 
 
-})
-EMINI.addEventListener("click", function(event){
-    event.preventDefault();
+// })
+// EMINI.addEventListener("click", function(event){
+//     event.preventDefault();
     
 
 
     
-container.innerHTML = `<h4>${marketData.quotes[2].shortname}</h4> 
-                        <p> Quote Type: ${marketData.quotes[2].quoteType}</p>
-                        <p> Symbol: ${marketData.quotes[2].symbol}</p>
-                        <p> Score: ${marketData.quotes[2].score} </p>`
+// container.innerHTML = `<h4>${marketData.quotes[2].shortname}</h4> 
+//                         <p> Quote Type: ${marketData.quotes[2].quoteType}</p>
+//                         <p> Symbol: ${marketData.quotes[2].symbol}</p>
+//                         <p> Score: ${marketData.quotes[2].score} </p>`
                     
 
 
-})
-VANGUARD.addEventListener("click", function(event){
-    event.preventDefault();
+// })
+// VANGUARD.addEventListener("click", function(event){
+//     event.preventDefault();
     
 
 
     
-container.innerHTML = `<h4>${marketData.quotes[3].longname}</h4> 
-                        <p> Quote Type: ${marketData.quotes[3].quoteType}</p>
-                        <p> Symbol:  ${marketData.quotes[3].symbol}</p>
-                        <p> Score: ${marketData.quotes[3].score} </p>`
+// container.innerHTML = `<h4>${marketData.quotes[3].longname}</h4> 
+//                         <p> Quote Type: ${marketData.quotes[3].quoteType}</p>
+//                         <p> Symbol:  ${marketData.quotes[3].symbol}</p>
+//                         <p> Score: ${marketData.quotes[3].score} </p>`
                     
 
 
-})
-STPK.addEventListener("click", function(event){
-    event.preventDefault();
+// })
+// STPK.addEventListener("click", function(event){
+//     event.preventDefault();
     
 
 
     
-container.innerHTML = `<h4>${marketData.quotes[4].longname}</h4> 
-                        <p>    Quote Type: ${marketData.quotes[4].quoteType}</p>
-                        <p> Symbol:  ${marketData.quotes[4].symbol}</p>
-                        <p> Score: ${marketData.quotes[4].score} </p>`
+// container.innerHTML = `<h4>${marketData.quotes[4].longname}</h4> 
+//                         <p>    Quote Type: ${marketData.quotes[4].quoteType}</p>
+//                         <p> Symbol:  ${marketData.quotes[4].symbol}</p>
+//                         <p> Score: ${marketData.quotes[4].score} </p>`
                     
 
 
-})
+// })
+
+var userNameInput = document.querySelector("#userName");
+var passwordInput = document.querySelector("#password");
+var signUpButton = document.querySelector("#sign-up");
+var msgDiv = document.querySelector("#msg");
+var userNameSpan = document.querySelector("#user-Name");
+var userPasswordSpan = document.querySelector("#user-password");
+renderLastRegistered();
+function displayMessage(type, message) {
+  msgDiv.textContent = message;
+  msgDiv.setAttribute("class", type);
+}
+function renderLastRegistered() {
+  var userNameInput = localStorage.getItem("email");
+  var password = localStorage.getItem("password");
+  if (!userNameInput || !password) {
+    return;
+  }
+  userNameSpan.textContent = userNameInput;
+  userPasswordSpan.textContent = password;
+}
+signUpButton.addEventListener("click", function(event) {
+  event.preventDefault();
+  var userNameInput = document.querySelector("#userName").value;
+  var password = document.querySelector("#password").value;
+  if (userNameInput === "") {
+    displayMessage("error", "User Name cannot be blank");
+  } else if (password === "") {
+    displayMessage("error", "Password cannot be blank");
+  } else {
+    displayMessage("success", "Registered successfully");
+    localStorage.setItem("userName", userNameInput);
+    localStorage.setItem("password", password);
+    renderLastRegistered();
+  }
+});
